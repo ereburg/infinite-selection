4194f29b75b518d65627bb86556306fbc00907c7
Eugene <eugene.kotsarev@gmail.com>
2019-12-23 20:05:45 +0300

    new update 1.3 replace search script, minor fixes.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,33 +15,60 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// Search
 
-	document.querySelector('#exampleInputText').oninput = function () {
-		let val = this.value.trim();
-		let elasticItems = document.querySelectorAll('.browse-item');
-		if (val != '') {
-			elasticItems.forEach(function (elem) {
-				if (elem.innerText.search(val) == -1) {
-					elem.classList.add('hide');
-					elem.innerHTML = elem.innerText;
-				}
-				else {
-					elem.classList.remove('hide');
-					let str = elem.innerText;
-					elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length);
-				}
-			});
-		}
-		else {
-			elasticItems.forEach(function (elem) {
-				elem.classList.remove('hide');
-				elem.innerHTML = elem.innerText;
-			});
-		}
-	};
+	// Фильтрация по ключевым словам в поиске
+	const inputSearch = document.querySelector('.search-input');
+	
+	inputSearch.addEventListener('keyup', () => {
+		const parentContainer = document.querySelector('.sneakers-browse-container');
+		const exampleItems = document.querySelectorAll('.browse-item');
+		let filter, itemTitle, isItemTitle;
+		filter = inputSearch.value.trim().toUpperCase();
+		
+		for (let i = 0; i < exampleItems.length; i++) {
+			itemTitle = exampleItems[i].querySelector(".releases-title");
+			isItemTitle = itemTitle.textContent || itemTitle.innerText;
+			let notFound = document.createElement('p');
+			notFound.textContent = 'Not Found :(';
 
-	function insertMark(string, pos, len) {
-		return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
-	}
+			if (isItemTitle.trim().toUpperCase().indexOf(filter) > -1) {
+				exampleItems[i].style.display = "";
+				// parentContainer.remove(notFound);
+			} else {
+				exampleItems[i].style.display = "none";
+				// parentContainer.append(notFound);
+			}
+
+
+		}
+	});
+
+	// document.querySelector('#exampleInputText').oninput = function () {
+	// 	let val = this.value.trim();
+	// 	let elasticItems = document.querySelectorAll('.browse-item');
+	// 	if (val != '') {
+	// 		elasticItems.forEach(function (elem) {
+	// 			if (elem.innerText.search(val) == -1) {
+	// 				elem.classList.add('hide');
+	// 				elem.innerHTML = elem.innerText;
+	// 			}
+	// 			else {
+	// 				elem.classList.remove('hide');
+	// 				let str = elem.innerText;
+	// 				elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length);
+	// 			}
+	// 		});
+	// 	}
+	// 	else {
+	// 		elasticItems.forEach(function (elem) {
+	// 			elem.classList.remove('hide');
+	// 			elem.innerHTML = elem.innerText;
+	// 		});
+	// 	}
+	// };
+
+	// function insertMark(string, pos, len) {
+	// 	return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
+	// }
 
 	// Validation
 
