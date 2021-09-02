6c24be9611f523d07a15ae59e44a84d2404a84f0
Eugene <eugene.kotsarev@gmail.com>
2021-09-03 01:05:18 +0300

    upd dependencies

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,22 +1,26 @@
 import sourcemaps from 'gulp-sourcemaps'
 import rename from 'gulp-rename'
-import sass from 'gulp-sass'
 import purgecss from 'gulp-purgecss'
 import autoprefixer from 'gulp-autoprefixer'
 import csso from 'gulp-csso'
 import { server } from './server.js';
 import pkg from 'gulp'
+import gulpSass from 'gulp-sass'
+import sass from 'sass'
+import Fibers from 'fibers'
+import tilde from 'node-sass-tilde-importer'
 
+const SCSS = gulpSass(sass)
 const {src, dest} = pkg
 
 export const styles = () => {
   return src(`${$.conf.app}/${$.conf.pathStyles}/*.scss`)
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      errorLogToConsole: true,
-      outputStyle: 'compressed'
-    }))
-    .on('error', console.error.bind(console))
+    .pipe(SCSS.sync({
+        importer: tilde,
+        includePaths: ['./node_modules'],
+        fiber: Fibers,
+      }).on('error', SCSS.logError))
     .pipe(purgecss({
       content: [`${$.conf.app}/${$.conf.htmlPages}/**/*.html`],
       whitelistPatterns: [/scroll/, /hide/, /active/, /hidden/, /added/]
