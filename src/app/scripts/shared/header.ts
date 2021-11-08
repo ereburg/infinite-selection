4a2b55ba802eb890de3505c0e28d5eaccbcb8f2b
diana.dyakovskaya <diana.dyakovskaya7@gmail.com>
2021-11-08 17:35:19 +0300

    add search

--- src/app/scripts/shared/header.ts
+++ src/app/scripts/shared/header.ts
@@ -16,5 +16,27 @@ export const Header = (): void => {
         header.classList.toggle('opened')
       }
     })
+
+    // Header search
+    const btnOpenSearch = document.querySelector('.js-header-search__open')
+    const headerSearch = document.querySelector('.js-header-search')
+    const headerInputSearch = document.querySelector('.js-header-search__input')
+    const btnCloseSearch = document.querySelector('.js-header-search__close')
+    const headerSearchResult = document.querySelector('.js-header-search__results')
+    btnOpenSearch.addEventListener('click', (event) => {
+      if ((event.target as HTMLElement).closest('.js-header-search__open')) {
+        headerSearch.classList.add('open')
+      }
+    })
+    btnCloseSearch.addEventListener('click', (event) => {
+      if ((event.target as HTMLElement).closest('.js-header-search__close')) {
+        headerSearch.classList.remove('open')
+      }
+    })
+    headerInputSearch.addEventListener('click', (event) => {
+      if ((event.target as HTMLElement).closest('.js-header-search__input')) {
+        headerSearchResult.classList.add('visible')
+      }
+    })
   }
 }
